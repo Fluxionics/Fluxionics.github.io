2b3066095ad81b5749369cd367746eb8b384b26b
Fluxionics <memitoyt1356@gmail.com>
2025-10-18 11:53:34 -0600

    Add config.js with access key and destination
    Added configuration details and a master password.

---
+++ config.js
@@ -0,0 +1,9 @@
+// ====================================================================
+// config.js - CLAVE DE ACCESO
+// 🚨 ¡CAMBIA ESTA CLAVE MANUALMENTE CADA VEZ QUE LA COMPARTAN! 🚨
+// ====================================================================
+
+const MASTER_PASSWORD = 'clave-secreta-para-acceso-v1';
+
+// El destino final después de pasar el captcha
+const FINAL_DESTINATION = 'index.html';
